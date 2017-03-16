63147ee15b30ea4f3190994e5d3c65748326c411
Serge ALEXANDRE <sa@broadsoftware.com>
2017-03-16 11:35:07 +0100

    Initial commit

---
+++ src/main/java/com/kappaware/jdchive/Main.java
@@ -0,0 +1,19 @@
+package com.kappaware.jdchive;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class Main {
+	static Logger log = LoggerFactory.getLogger(Main.class);
+
+	static public void main(String[] argv) throws IOException {
+		
+	}
+	
+}
+
+
